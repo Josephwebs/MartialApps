43942d9962363691255d089470749b326c17f713
JosephWebs <jose_luis967@hotmail.com>
2024-06-08 12:43:11 -0400

    cambios esteticos

--- src/app/pages/clases-detalle/clases-detalle.page.ts
+++ src/app/pages/clases-detalle/clases-detalle.page.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import {
+  ModalController,
+  ToastController,
+  LoadingController,
+} from '@ionic/angular';
 import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -14,15 +18,21 @@ export class ClasesDetallePage {
   constructor(
     private modalController: ModalController,
     private apiService: ApiService,
-    private router: Router
+    private router: Router,
+    private toastController: ToastController,
+    private loadingController: LoadingController
   ) {}
 
-  ngOnInit() {
-    this.clase = this.router.getCurrentNavigation().extras.state.clase;
-    console.log(this.clase);
+  async ngOnInit() {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation.extras.state && navigation.extras.state.clase) {
+      this.clase = navigation.extras.state.clase;
+    } else {
+      // handle no state passed
+    }
   }
 
-  tomarClase() {
+  async tomarClase() {
     const usuario = JSON.parse(localStorage.getItem('user'));
     const solicitud = {
       clase_id: this.clase.id,
@@ -32,20 +42,39 @@ export class ClasesDetallePage {
       hora: this.clase.df_hora,
     };
 
+    const loading = await this.loadingController.create({
+      message: 'Procesando solicitud...',
+    });
+    await loading.present();
+
     this.apiService
       .reservarClase(solicitud)
-      .then((response) => {
+      .then(async (response) => {
         if (response.ok) {
-          alert('Solicitud de reserva enviada');
+          await loading.dismiss();
+          this.presentToast('Solicitud de reserva enviada', 'success');
           this.router.navigate(['principal']);
         } else {
-          alert('Error al enviar la solicitud');
+          await loading.dismiss();
+          this.presentToast('Error al enviar la solicitud', 'danger');
         }
         this.modalController.dismiss();
       })
-      .catch((error) => {
-        alert('Error al enviar la solicitud');
+      .catch(async (error) => {
+        await loading.dismiss();
+        this.presentToast('Error al enviar la solicitud', 'danger');
         this.modalController.dismiss();
       });
+  }
+
+  async presentToast(mensaje, color) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 1700,
+      position: 'middle',
+      color: color,
+    });
+
+    await toast.present();
   }
 }
